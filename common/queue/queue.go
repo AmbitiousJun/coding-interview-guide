cde41a845c81805325cbd79362fb5c6f59c82105
ambitious <1535820771@qq.com>
2024-07-22 10:33:24 +0800

    C1-T4

--- common/queue/queue.go
+++ common/queue/queue.go
@@ -14,39 +14,39 @@ func New[T any](typeElm T) *Q[T] {
 }
 
 // Empty 检查是否栈空
-func (s *Q[T]) Empty() bool {
-	return s.Len() == 0
+func (q *Q[T]) Empty() bool {
+	return q.Len() == 0
 }
 
 // Len 获取栈大小
-func (s *Q[T]) Len() int {
-	return len(s.arr)
+func (q *Q[T]) Len() int {
+	return len(q.arr)
 }
 
 // Add 元素入队
-func (s *Q[T]) Add(elm T) {
-	s.arr = append(s.arr, elm)
+func (q *Q[T]) Add(elm T) {
+	q.arr = append(q.arr, elm)
 }
 
 // Poll 队首元素出队
-func (s *Q[T]) Poll() T {
-	if s.Empty() {
+func (q *Q[T]) Poll() T {
+	if q.Empty() {
 		panic("empty queue")
 	}
-	elm := s.arr[0]
-	s.arr = s.arr[1:]
+	elm := q.arr[0]
+	q.arr = q.arr[1:]
 	return elm
 }
 
 // Peek 获取队首元素
-func (s *Q[T]) Peek() T {
-	if s.Empty() {
+func (q *Q[T]) Peek() T {
+	if q.Empty() {
 		panic("empty queue")
 	}
-	return s.arr[0]
+	return q.arr[0]
 }
 
 // String 返回栈中元素
-func (s *Q[T]) String() string {
-	return fmt.Sprintf("%v", s.arr)
+func (q *Q[T]) String() string {
+	return fmt.Sprintf("%v", q.arr)
 }
