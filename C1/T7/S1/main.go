a5b179e3f21160aaae53bfeda8ca25b19fb65e17
ambitious <1535820771@qq.com>
2024-07-23 10:29:15 +0800

    C1-T7

---
+++ C1/T7/S1/main.go
@@ -0,0 +1,47 @@
+// 解法1
+// 准备一个 qmax 双端队列
+// 遍历数组 arr 元素, 对于每一个元素所在位置 i, 判断 qmax 队尾元素 j
+//
+//	① arr[i] < arr[j]: i 直接入 qmax 队尾
+//	② arr[i] >= arr[j]: 不断弹出 qmax 队尾, 直至满足条件 ① 或者 qmax 队空为止
+//
+// 每次遍历时, 判断 qmax 队首元素 first 是否满足 first == i - w, w 为窗口大小
+// 满足则弹出队首
+package main
+
+import (
+	"coding_interview_guide/common/deque"
+	"coding_interview_guide/common/integer"
+	"fmt"
+)
+
+func main() {
+	res := GetMaxWindow([]int{4, 3, 5, 4, 3, 3, 6, 7}, 3)
+	fmt.Println("res: ", res)
+}
+
+func GetMaxWindow(arr []int, w int) []int {
+	if arr == nil || w < 1 || len(arr) < w {
+		return nil
+	}
+	qmax := deque.New(0)
+	res := make([]int, len(arr)-w+1)
+	var index integer.I = 0
+	for i, num := range arr {
+		// 1 qmax 不为空的情况下, 弹出队尾不满足条件的元素
+		for !qmax.Empty() && arr[qmax.PeekLast()] <= num {
+			qmax.PollLast()
+		}
+		// 2 当前元素入队
+		qmax.AddLast(i)
+		// 3 去掉队首的不满足条件元素
+		if qmax.PeekFirst() == i-w {
+			qmax.PollFirst()
+		}
+		// 4 当前已经至少遍历了一个窗口大小, 可以开始更新 res
+		if i >= w-1 {
+			res[index.GetAndIncr()] = arr[qmax.PeekFirst()]
+		}
+	}
+	return res
+}
---
+++ common/deque/deque.go
@@ -0,0 +1,75 @@
+// 通用双端队列结构实现
+package deque
+
+import "fmt"
+
+type D[T any] struct {
+	arr []T
+}
+
+// New 初始化一个通用双端队列结构
+// 传入指定类型的任意值确定泛型
+func New[T any](typeElm T) *D[T] {
+	return &D[T]{arr: []T{}}
+}
+
+// Empty 检查是否栈空
+func (d *D[T]) Empty() bool {
+	return d.Len() == 0
+}
+
+// Len 获取栈大小
+func (d *D[T]) Len() int {
+	return len(d.arr)
+}
+
+// AddLast 元素入队尾
+func (d *D[T]) AddLast(elm T) {
+	d.arr = append(d.arr, elm)
+}
+
+// AddFirst 元素入队首
+func (d *D[T]) AddFirst(elm T) {
+	d.arr = append([]T{elm}, d.arr...)
+}
+
+// PollLast 队首元素出队尾
+func (d *D[T]) PollLast() T {
+	if d.Empty() {
+		panic("empty deque")
+	}
+	elm := d.arr[d.Len()-1]
+	d.arr = d.arr[:d.Len()-1]
+	return elm
+}
+
+// PollFirst 队首元素出队首
+func (d *D[T]) PollFirst() T {
+	if d.Empty() {
+		panic("empty deque")
+	}
+	elm := d.arr[0]
+	d.arr = d.arr[1:]
+	return elm
+}
+
+// PeekLast 获取队尾元素
+func (d *D[T]) PeekLast() T {
+	if d.Empty() {
+		panic("empty deque")
+	}
+	return d.arr[d.Len()-1]
+}
+
+// PeekFirst 获取队首元素
+func (d *D[T]) PeekFirst() T {
+	if d.Empty() {
+		panic("empty deque")
+	}
+	return d.arr[0]
+}
+
+// String 返回栈中元素
+func (d *D[T]) String() string {
+	return fmt.Sprintf("%v", d.arr)
+}
